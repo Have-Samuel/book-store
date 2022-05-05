1429e5fe1647fb9c183b9957ca1017f49fe8fa97
Have Samuel <samiehave9@gmail.com>
2022-05-05 15:58:04 +0300

    Added a getter of a book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,31 @@
 import { v4 as uuid } from 'uuid';
 
+const appId = 'NXcpJ7tVwe6VklFiOYLn';
+const BooksUrl = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}/books`;
+
 const ADD_BOOK = './books/ADD_BOOK';
 const REMOVE_BOOK = './books/REMOVE_BOOK';
+const GET_BOOKS = './books/GET_BOOKS';
 
-// set initial default state for the shelf
-const initialState = {
-  books: [],
+export const fetchBooks = async () => {
+  const response = await fetch(
+    BooksUrl,
+  );
+  const data = await response.json();
+  const books = [];
+  Object.entries(data).forEach((book) => {
+    books.push(...book[1]);
+    console.log({ ...book[1], id: book[0] });
+  });
+  return books;
+};
+
+export const getBooksFromApi = () => async (dispatch) => {
+  const books = await fetchBooks();
+  dispatch({
+    type: GET_BOOKS,
+    payload: books,
+  });
 };
 
 export const addBook = (newTitle, newAuthor) => {
@@ -14,6 +34,10 @@ export const addBook = (newTitle, newAuthor) => {
     type: ADD_BOOK,
     payload: { id: newID, title: newTitle, author: newAuthor },
   };
+};
+
+const initialState = {
+  books: [],
 };
 
 export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
@@ -28,6 +52,9 @@ export default (state = initialState, action) => {
         (book) => book.id !== action.payload,
       );
       return { ...state, books: [...filteredBooks] };
+    }
+    case GET_BOOKS: {
+      return { ...state, books: [...action.payload] };
     }
     default:
       return state;
