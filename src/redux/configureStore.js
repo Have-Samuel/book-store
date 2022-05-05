590e47f41d0a21411bd29456200b760ea66ac7df
Have Samuel <samiehave9@gmail.com>
2022-05-05 13:53:57 +0300

    Installed the thunk and the applyMiddleWare

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import Books from './books/books';
 import categories from './categories/categories';
 
 const rootReducer = combineReducers({ bookshelf: Books, checkStatus: categories });
-
-const store = configureStore({ reducer: rootReducer });
+const middleWare = applyMiddleware(thunk);
+const store = configureStore({ reducer: rootReducer }, middleWare);
 
 export default store;
