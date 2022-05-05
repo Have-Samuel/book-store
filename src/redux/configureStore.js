1043bd97bc77b024fb606f38bff4119d49a76b1a
Have Samuel <samiehave9@gmail.com>
2022-05-05 14:53:50 +0300

    Installed the softwares

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
-import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { applyMiddleware } from 'redux';
 import Books from './books/books';
 import categories from './categories/categories';
 
 const rootReducer = combineReducers({ bookshelf: Books, checkStatus: categories });
-const middleWare = applyMiddleware(thunk);
+const middleWare = applyMiddleware(thunk, logger);
 const store = configureStore({ reducer: rootReducer }, middleWare);
 
 export default store;
